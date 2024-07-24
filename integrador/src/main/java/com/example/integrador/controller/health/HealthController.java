af816b4c11e225bb25118fe68f36990d44af7262
Sebsss10 <hernandezsebastian1796@gmail.com>
2024-07-24 10:41:47 -0600

    Configuracion de Swagger

--- integrador/src/main/java/com/example/integrador/controller/health/HealthController.java
+++ integrador/src/main/java/com/example/integrador/controller/health/HealthController.java
@@ -2,7 +2,7 @@ package com.example.integrador.controller.health;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import jakarta.servlet.http.HttpServletRequest;
 @RestController
 public class HealthController {
 
